a92cb29098d39d9d0de4c3c8c65c70ac79a060de
Max Coronado <mcoronado@Majordrilling.com>
2025-04-25 14:54:35 -0400

    More attemps

--- itinman/pages/index.py
+++ itinman/pages/index.py
@@ -1,4 +1,26 @@
 import reflex as rx
 
+def side_panel() -> rx.Component:
+    return rx.vstack(
+        rx.text('{ itinman }', color='black', size='3'),
+        rx.spacer(),
+        align='start',
+        width='10%',
+        padding='0.5em',
+        background_color='gray.100',
+    )
+
+def main_panel() -> rx.Component:
+    pass
+
+
 def index() -> rx.Component:
-    return rx.text("itinman")
+    return rx.hstack(
+        side_panel(),
+        main_panel(),
+        width='100%',
+        height='100%',
+        padding='0.5em',
+        spacing='0.5em',
+        background_color='white',
+    )
