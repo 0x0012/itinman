dbefbc72cd96bd852f0124fd263ce663104a709a
0x0012 <xiomaxx@hotmail.com>
2025-05-18 02:54:36 -0400

    playing

--- itinman/itinman.py
+++ itinman/itinman.py
@@ -1,21 +1,7 @@
 import reflex as rx
-from .pages import *
+from .pages.index import index
 
-base_stylesheets = [
-    "https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap",
-]
-
-base_style = {
-    "font_family": "Inter",
-}
-
-app = rx.App(
-    style=base_style,
-    stylesheets=base_stylesheets,
-    theme=rx.theme(
-        appearance="light", has_background=True, radius="large", accent_color="orange"
-    ),
-)
+app = rx.App()
 
 app.add_page(
     index,
--- itinman/pages/index.py
+++ itinman/pages/index.py
@@ -1,26 +1,50 @@
 import reflex as rx
+from reflex.components.radix.themes.layout.stack import hstack
+
+md_red = '#a90533'
+md_grey = '#5f6062'
+
+def itinman_logo() -> rx.Component:
+    return rx.heading(
+        rx.text('{itin', as_='span', color=md_grey),
+        rx.text('man', as_='span', color=md_red),
+        rx.text('}', as_='span', color=md_grey)
+    )
+
+def header() -> rx.Component:
+    return rx.hstack(
+        itinman_logo()
+    )
 
 def side_panel() -> rx.Component:
     return rx.vstack(
-        rx.text('{ itinman }', color='black', size='3'),
-        rx.spacer(),
-        align='start',
-        width='10%',
-        padding='0.5em',
-        background_color='gray.100',
+        rx.text('side panel'),
+        width = '10vw'
     )
 
 def main_panel() -> rx.Component:
-    pass
+    return rx.vstack(
+        rx.text('main panel'),
+        width = '90vw'
+    )
 
+def footer() -> rx.Component:
+    return rx.center(
+        rx.text('by 0x0012', align='center', weight='light', color=md_grey),
+        width = '100%'
+    )
 
 def index() -> rx.Component:
-    return rx.hstack(
-        side_panel(),
-        main_panel(),
-        width='100%',
-        height='100%',
-        padding='0.5em',
-        spacing='0.5em',
-        background_color='white',
+    return rx.vstack(
+        header(),
+        hstack(
+            side_panel(),
+            main_panel(),
+            widht = '100%',
+            height = '100%'
+        ),
+        footer(),
+        height = '100vh',
+        background = 'white',
+        padding = '1.5em'
     )
